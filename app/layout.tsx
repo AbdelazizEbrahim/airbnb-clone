ebf99af72c6bf9a68311233a343c6c4b071a1a85
Abdelaziz Ebrahim <abdelazizebrahim5980@gmail.com>
2024-11-12 10:03:34 -0800

    Nav bar UI completed

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+
+export const metadata = {
+  title: "Airbnb clone",
+  description: "Airbnb clone Ethiopia"
+}
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${font.className}`}
+      >
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
